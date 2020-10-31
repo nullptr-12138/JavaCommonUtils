7cac67241c0c6c2805a7082e8817e4ec130e5c8c
nullptr <2281525307@qq.com>
2020-10-31 12:28:45 +0800

    增加正则表达式工具，集成常用正则表达式

--- src/main/java/com/nullptr/utils/character/StringUtils.java
+++ src/main/java/com/nullptr/utils/character/StringUtils.java
@@ -1,33 +1,14 @@
 package com.nullptr.utils.character;
 
-import java.util.regex.Pattern;
-
 /**
  * 字符串语言验证工具，用于判断字符串是否为中文或英文
- *
- *
  *
  * @author 胖橘
  * @version 1.0 2020-10-9
  * @since 1.0 2020-10-9
  */
-public class StringUtils {
-    private static final Pattern ENGLISH = Pattern.compile("[a-zA-Z ]*");
-    private static final Pattern CHINESE = Pattern.compile("[\\u4e00-\\u9fa5 ]*");
-
+public class StringUtils extends org.apache.commons.lang3.StringUtils {
     private StringUtils() {
-    }
-
-    public static boolean isEnglish(String word) {
-        return ENGLISH.matcher(word).matches();
-    }
-
-    public static boolean isChinese(String word) {
-        return CHINESE.matcher(word).matches();
-    }
-
-    public static boolean isEmpty(CharSequence ch){
-        return ch == null || ch.length() == 0;
     }
 
 }
